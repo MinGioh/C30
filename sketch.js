3edd4fed8ac0e742ca11ec66485722287ca914cd
MinGioh <98238961+MinGioh@users.noreply.github.com>
2022-05-02 16:20:34 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,13 +10,13 @@ const Composite = Matter.Composite;
 let engine;
 let world;
 var ground;
-var fruit,rope;
-var link;
+var fruit,rope,rope2,rope3;
+var link, link2, link3;
 
 var bg_img;
 var food;
 var rabbit, rabbitIMG;
-var b1;
+var b1, b2, b3;
 var piscando,comendo,triste;
 var somFundo, somTriste, somCorda, somComendo, somAr;
 var soprador;
@@ -79,7 +79,9 @@ function setup()
   ground = new Ground(200,canH,600,20);
 
   //criação do objeto corda a partir da classe 
-  rope = new Rope(7,{x:245,y:30});
+  rope = new Rope(5,{x:245,y:30});
+  rope2 = new Rope(8,{x:350, y:10});
+  rope3 = new Rope(7,{x:180, y:20});
 
   //criação do corpo fruta e adição ao composto 
   fruit = Bodies.circle(300,300,20);
@@ -90,6 +92,16 @@ function setup()
   b1.position(220,30);
   b1.size(50,50);
   b1.mouseClicked(cair);
+
+  b2 = createImg('cut_btn.png');
+  b2.position(330,10);
+  b2.size(50,50);
+  b2.mouseClicked(cair2);
+
+  b3 = createImg('cut_btn.png');
+  b3.position(160,20);
+  b3.size(50,50);
+  b3.mouseClicked(cair3);
 
   //criação do botão para soprar a fruta
   soprador = createImg('balloon.png');
@@ -112,6 +124,8 @@ function setup()
 
   //criação do objeto link a partir da classe 
   link = new Link(rope,fruit);
+  link2 = new Link(rope2,fruit);
+  link3 = new Link(rope3,fruit);
 
   //configuração de imagem, desenhos e texto
   rectMode(CENTER);
@@ -131,6 +145,8 @@ function draw()
     image(food,fruit.position.x,fruit.position.y,70,70);
   }
   rope.show();
+  rope2.show();
+  rope3.show();
   Engine.update(engine);
   //ground.show();
 
@@ -163,6 +179,22 @@ function cair()
   rope.break();
   link.cortar();
   link = null;
+}
+
+function cair2()
+{
+  somCorda.play();
+  rope2.break();
+  link2.cortar();
+  link2 = null;
+}
+
+function cair3()
+{
+  somCorda.play();
+  rope3.break();
+  link3.cortar();
+  link3 = null;
 }
 
 function soprar()
